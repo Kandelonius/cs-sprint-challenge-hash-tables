839b25cce6b75a6beac0e425bbd35996c172f07d
Shane Kaestner <shane.kaestner@yahoo.com>
2020-09-04 10:21:55 -0700

    completed ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,28 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    connections = {}
+    route = []
+    # print(f"first ticket is {tickets[0]}")
+    for ticket in tickets:
+        # print(ticket)
+        connections[ticket.source] = ticket.destination
+        # putting the first element into the list.
+        if ticket.source == "NONE":
+            route.append(ticket.destination)
+    # add the items from connections to the list.
+    for i in range(length - 1):
+        temp = connections[route[i]]
+        route.append(temp)
 
+    print(connections)
     return route
+#
+# ticket_1 = Ticket("NONE", "PDX")
+# ticket_2 = Ticket("PDX", "DCA")
+# ticket_3 = Ticket("DCA", "NONE")
+#
+# tickets = [ticket_1, ticket_2, ticket_3]
+#
+# # expected = ["PDX", "DCA", "NONE"]
+# print(reconstruct_trip(tickets, 3))
