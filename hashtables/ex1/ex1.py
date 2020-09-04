a5be8a00ee3572b8143a185a6f150e16fd710fcf
Shane Kaestner <shane.kaestner@yahoo.com>
2020-09-04 09:45:34 -0700

    completed ex 1.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,17 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
+    totals = {}
+    count = 0
+    # What if we store each weight in the input list as keys? What would be a
+    # useful thing to store as the value for each key?
+    for num in weights:
+        # if num == limit:
+        #     return num
+        if (limit - num) in totals:
+            result = (count, (totals[limit - num]))
+            return result
+        totals[num] = count
+        count += 1
+    # print(totals)
     return None
+weights_3 = [4, 6, 10, 15, 16]
+print(get_indices_of_item_weights(weights_3, 5, 21))
